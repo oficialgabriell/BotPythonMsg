cecb97584e6a86dc218486e7890c6fc459670754
Denilson <fcodenilson@gmail.com>
2024-12-13 20:15:05 -0300

    Add environment configuration and update automation script
    - Introduced .env and .gitignore files for environment variable management.
    - Updated main.py to load environment variables for file paths and browser settings.
    - Modified the script to use dynamic paths for the Excel file and PNG image.
    - Enhanced WhatsApp messaging functionality by formatting messages from the spreadsheet.

--- AUTOMACAO/main.py
+++ AUTOMACAO/main.py
@@ -1,24 +1,49 @@
+from urllib.parse import quote
+import os  # Para executar comandos no sistema operacional
+import time  # Para controlar o tempo de espera
 import pandas as pd  # Bibliotecas necessárias
 import pyautogui  # Para automação de clique e digitação
-import time  # Para controlar o tempo de espera
 import webbrowser  # Para abrir URLs no navegador
-import os  # Para executar comandos no sistema operacional
 import pygetwindow as gw  # Para manipular as janelas abertas no sistema
+from dotenv import load_dotenv
 
+load_dotenv()
+
+CAMINHO_PLANILHA = os.environ.get(
+    "CAMINHO_PLANILHA",
+    None,  # Caminho do arquivo Excel definido no arquivo .env
+)
+
+CAMINHO_PNG = os.environ.get(
+    "CAMINHO_PNG",
+    None,
+)
+
+BROWSER_PATH = os.environ.get(
+    "BROWSER_PATH",
+    None,
+)
+
+# print(os.getenv("PYTHONPATH"))
+# print(os.getenv("CAMINHO_PLANILHA"))
+# print(os.getenv("CAMINHO_PNG"))
 
 # Carregar a planilha com os dados de WhatsApp
-arquivo = "P1.xlsx"  # Caminho do arquivo Excel
+arquivo = CAMINHO_PLANILHA
 planilha = pd.read_excel(arquivo)
+print(planilha)
 
 # Verificar se a coluna 'LINK DO WHATSAPP' existe na planilha
-if 'LINK DO WHATSAPP' not in planilha.columns:
-    raise ValueError("A coluna 'LINK DO WHATSAPP' não foi encontrada na planilha.")
+# if "LINK DO WHATSAPP" not in planilha.columns:
+# raise ValueError("A coluna 'LINK DO WHATSAPP' não foi encontrada na planilha.")
+
 
 # Função para clicar no botão 'Iniciar conversa' após abrir o link
 def clicar_iniciar_conversa():
     # Espera um tempo para garantir que a página carregue completamente
     print("Aguardando a página carregar...")
     time.sleep(10)  # Esperar 10 segundos
+
 
 # Função para enviar a mensagem
 def enviar_mensagem(mensagem):
@@ -28,7 +53,9 @@ def enviar_mensagem(mensagem):
 
     try:
         # Localizar o campo de digitação da mensagem usando uma imagem do campo de texto
-        campo_pos = pyautogui.locateOnScreen('campo_texto.png', confidence=0.8)  # Ajuste a imagem conforme necessário
+        campo_pos = pyautogui.locateOnScreen(
+            CAMINHO_PNG, confidence=0.6
+        )  # Ajuste a imagem conforme necessário
 
         if campo_pos:
             # Se o campo for encontrado, clicamos nele
@@ -41,18 +68,23 @@ def enviar_mensagem(mensagem):
             print("Mensagem digitada...")
 
             # Espera um pouco e envia a mensagem pressionando Enter
-            time.sleep(2)  # Aguardar 2 segundos para garantir que a mensagem foi digitada
-            pyautogui.press('enter')  # Simula pressionar a tecla Enter para enviar a mensagem
+            time.sleep(
+                2
+            )  # Aguardar 2 segundos para garantir que a mensagem foi digitada
+            pyautogui.press(
+                "enter"
+            )  # Simula pressionar a tecla Enter para enviar a mensagem
             print("Mensagem enviada!")
         else:
             print("Não foi possível localizar o campo de digitação.")
     except Exception as e:
         print(f"Erro ao localizar o campo de digitação: {e}")
 
+
 # Função para focar na janela do WhatsApp Web
 def focar_na_janela_whatsapp():
     # Procurar por janelas abertas com o título 'WhatsApp'
-    janelas = gw.getWindowsWithTitle('WhatsApp')  # O título da janela do WhatsApp Web
+    janelas = gw.getWindowsWithTitle("WhatsApp")  # O título da janela do WhatsApp Web
     print("Verificando janelas abertas...")
 
     if janelas:
@@ -63,21 +95,23 @@ def focar_na_janela_whatsapp():
     else:
         print("Não encontramos o WhatsApp Web aberto.")
 
+
 # Iterar sobre os links de WhatsApp na planilha
-for i, link in enumerate(planilha['LINK DO WHATSAPP']):
-    if pd.notnull(link):  # Verifica se o link não está vazio
-        print(f"Abrindo o link {i + 1}: {link}")
-
-        # Alterar o link para o formato compatível com o WhatsApp Web
-        link_whatsapp_web = link.replace("https://wa.me/", "https://web.whatsapp.com/send?phone=")
-
-        # Caminho para o executável do Opera GX (ajuste o caminho conforme necessário)
-        opera_path = r"C:\Users\win10\AppData\Local\Programs\Opera GX\opera.exe"
+for i, mensagem in enumerate(planilha["MENSAGEM"]):
+    if pd.notnull(mensagem):  # Verifica se o link não está vazio
+        mensagem_formatada = quote(mensagem)
+        numero = planilha["NÚMERO"][i]
+        link_whatsapp_web = (
+            f"https://web.whatsapp.com/send?phone={numero}&text={mensagem_formatada}"
+        )
+        print(f"Abrindo o link {i + 1}: {link_whatsapp_web}")
 
         # Abrir o link no Opera GX (ou outro navegador)
         print("Abrindo o WhatsApp Web no navegador...")
-        os.system(f'"{opera_path}" {link_whatsapp_web}')
-        time.sleep(10)  # Espera a página carregar (pode ser ajustado conforme necessário)
+        os.system(f'"{BROWSER_PATH}" {link_whatsapp_web}')
+        time.sleep(
+            10
+        )  # Espera a página carregar (pode ser ajustado conforme necessário)
 
         # Focar na janela do WhatsApp Web, caso esteja aberta
         focar_na_janela_whatsapp()
@@ -85,13 +119,12 @@ for i, link in enumerate(planilha['LINK DO WHATSAPP']):
         # Chama a função para clicar no botão "Iniciar conversa"
         clicar_iniciar_conversa()
 
-        # Obtém a mensagem da planilha e envia para o link do WhatsApp
-        mensagem = planilha['MENSAGEM'][i]  # Obtém a mensagem da planilha
+        # Envia a mensagem da planilha para o whatsapp
         print(f"Enviando a mensagem: {mensagem}")
         enviar_mensagem(mensagem)
 
         # Fechar a aba atual do navegador após enviar a mensagem
-        pyautogui.hotkey('ctrl', 'w')  # Fecha a aba do navegador
+        pyautogui.hotkey("ctrl", "w")  # Fecha a aba do navegador
         time.sleep(5)  # Pausa para garantir o fechamento da aba
 
 # Mensagem final indicando que o processo foi concluído
