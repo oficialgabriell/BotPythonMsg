a6b1da2f360d00e89e42b58dbeb3b5d3857907d4
denilson <fcodenilson@gmail.com>
2024-12-14 07:49:48 -0300

    Refactor WhatsApp automation script and update requirements

--- AUTOMACAO/main.py
+++ AUTOMACAO/main.py
@@ -3,7 +3,7 @@ import os  # Para executar comandos no sistema operacional
 import time  # Para controlar o tempo de espera
 import pandas as pd  # Bibliotecas necessárias
 import pyautogui  # Para automação de clique e digitação
-import webbrowser  # Para abrir URLs no navegador
+import webbrowser  # Para abrir o navegador
 import pygetwindow as gw  # Para manipular as janelas abertas no sistema
 from dotenv import load_dotenv
 
@@ -24,6 +24,8 @@ BROWSER_PATH = os.environ.get(
     None,
 )
 
+webbrowser.register("chrome", None, webbrowser.BackgroundBrowser(BROWSER_PATH))
+
 # print(os.getenv("PYTHONPATH"))
 # print(os.getenv("CAMINHO_PLANILHA"))
 # print(os.getenv("CAMINHO_PNG"))
@@ -31,18 +33,21 @@ BROWSER_PATH = os.environ.get(
 # Carregar a planilha com os dados de WhatsApp
 arquivo = CAMINHO_PLANILHA
 planilha = pd.read_excel(arquivo)
-print(planilha)
-
-# Verificar se a coluna 'LINK DO WHATSAPP' existe na planilha
-# if "LINK DO WHATSAPP" not in planilha.columns:
-# raise ValueError("A coluna 'LINK DO WHATSAPP' não foi encontrada na planilha.")
 
 
-# Função para clicar no botão 'Iniciar conversa' após abrir o link
-def clicar_iniciar_conversa():
-    # Espera um tempo para garantir que a página carregue completamente
-    print("Aguardando a página carregar...")
-    time.sleep(10)  # Esperar 10 segundos
+# Função para focar na janela do WhatsApp Web
+def focar_na_janela_whatsapp():
+    # Procurar por janelas abertas com o título 'WhatsApp'
+    janelas = gw.getWindowsWithTitle("WhatsApp")  # O título da janela do WhatsApp Web
+    print("Verificando janelas abertas...") 
+
+    if janelas:
+        # Se encontrar uma janela do WhatsApp, foca nela
+        janela = janelas[0]
+        janela.activate()  # Ativa a janela do WhatsApp Web
+        print("Janela do WhatsApp Web ativada.")
+    else:
+        print("Não encontramos o WhatsApp Web aberto.")
 
 
 # Função para enviar a mensagem
@@ -52,9 +57,9 @@ def enviar_mensagem(mensagem):
     time.sleep(5)  # Aguardar 5 segundos para o campo de digitação estar visível
 
     try:
-        # Localizar o campo de digitação da mensagem usando uma imagem do campo de texto
+    # Localizar o campo de digitação da mensagem usando uma imagem do campo de texto
         campo_pos = pyautogui.locateOnScreen(
-            CAMINHO_PNG, confidence=0.6
+            CAMINHO_PNG, confidence=0.9
         )  # Ajuste a imagem conforme necessário
 
         if campo_pos:
@@ -63,37 +68,15 @@ def enviar_mensagem(mensagem):
             pyautogui.click(campo_centro)  # Clica no campo de texto
             print("Campo de mensagem selecionado.")
 
-            # Digitar a mensagem no campo
-            pyautogui.write(mensagem)  # Digita a mensagem no campo
-            print("Mensagem digitada...")
-
-            # Espera um pouco e envia a mensagem pressionando Enter
-            time.sleep(
-                2
-            )  # Aguardar 2 segundos para garantir que a mensagem foi digitada
             pyautogui.press(
                 "enter"
             )  # Simula pressionar a tecla Enter para enviar a mensagem
             print("Mensagem enviada!")
         else:
             print("Não foi possível localizar o campo de digitação.")
-    except Exception as e:
-        print(f"Erro ao localizar o campo de digitação: {e}")
-
-
-# Função para focar na janela do WhatsApp Web
-def focar_na_janela_whatsapp():
-    # Procurar por janelas abertas com o título 'WhatsApp'
-    janelas = gw.getWindowsWithTitle("WhatsApp")  # O título da janela do WhatsApp Web
-    print("Verificando janelas abertas...")
-
-    if janelas:
-        # Se encontrar uma janela do WhatsApp, foca nela
-        janela = janelas[0]
-        janela.activate()  # Ativa a janela do WhatsApp Web
-        print("Janela do WhatsApp Web ativada.")
-    else:
-        print("Não encontramos o WhatsApp Web aberto.")
+    except pyautogui.ImageNotFoundException as e:
+        print(f"Não foi possível localizar a imagem.")
+        raise e
 
 
 # Iterar sobre os links de WhatsApp na planilha
@@ -108,16 +91,13 @@ for i, mensagem in enumerate(planilha["MENSAGEM"]):
 
         # Abrir o link no Opera GX (ou outro navegador)
         print("Abrindo o WhatsApp Web no navegador...")
-        os.system(f'"{BROWSER_PATH}" {link_whatsapp_web}')
+        webbrowser.get("chrome").open(link_whatsapp_web)
         time.sleep(
             10
         )  # Espera a página carregar (pode ser ajustado conforme necessário)
 
-        # Focar na janela do WhatsApp Web, caso esteja aberta
+            # Focar na janela do WhatsApp Web, caso esteja aberta
         focar_na_janela_whatsapp()
-
-        # Chama a função para clicar no botão "Iniciar conversa"
-        clicar_iniciar_conversa()
 
         # Envia a mensagem da planilha para o whatsapp
         print(f"Enviando a mensagem: {mensagem}")
