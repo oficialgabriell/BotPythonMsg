8be51f138f8140b703f17fea7ae71aa2c4a2e0e3
Denilson <fcodenilson@gmail.com>
2024-12-14 10:34:24 -0300

    Refactor WhatsApp automation script to use Selenium for message sending and add browser utility functions

--- AUTOMACAO/main.py
+++ AUTOMACAO/main.py
@@ -1,11 +1,14 @@
 import webbrowser  # Para abrir o navegador
-import pygetwindow as gw  # Para manipular as janelas abertas no sistema
 from urllib.parse import quote
 import os  # Para executar comandos no sistema operacional
 import time  # Para controlar o tempo de espera
 import pandas as pd  # Bibliotecas necessárias
 import pyautogui  # Para automação de clique e digitação
 from dotenv import load_dotenv
+from utils.browser import get_browser
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
 load_dotenv()
 
@@ -24,7 +27,15 @@ BROWSER_PATH = os.environ.get(
     None,
 )
 
-webbrowser.register("chrome", None, webbrowser.BackgroundBrowser(BROWSER_PATH))
+PROFILE_CHROME_PATH = os.environ.get(
+    "PROFILE_CHROME_PATH",
+    None,
+)
+
+browser = get_browser(
+    rf"user-data-dir={PROFILE_CHROME_PATH}",
+    "--headless",
+)
 
 # Carregar a planilha com os dados de WhatsApp
 arquivo = CAMINHO_PLANILHA
@@ -33,26 +44,23 @@ planilha = pd.read_excel(arquivo)
 
 # Função para enviar a mensagem
 def enviar_mensagem(mensagem):
-    # Esperar um tempo para garantir que o campo de digitação esteja visível
     print("Localizando o campo de digitação para a mensagem...")
-    time.sleep(5)  # Aguardar 5 segundos para o campo de digitação estar visível
 
     try:
         # Localizar o campo de digitação da mensagem usando uma imagem do campo de texto
-        campo_pos = pyautogui.locateOnScreen(
-            CAMINHO_PNG, confidence=0.6
-        )  # Ajuste a imagem conforme necessário
+        inputMensagem = browser.find_element(
+            By.XPATH, '//*[@id="main"]/footer/div[1]/div/span/div/div[2]/div[1]/div[2]'
+        )
 
-        if campo_pos:
+        if inputMensagem:
             # Se o campo for encontrado, clicamos nele
-            campo_centro = pyautogui.center(campo_pos)
-            pyautogui.click(campo_centro)  # Clica no campo de texto
-            print("Campo de mensagem selecionado.")
-
-            pyautogui.press(
-                "enter"
-            )  # Simula pressionar a tecla Enter para enviar a mensagem
-            print("Mensagem enviada!")
+            inputMensagem.click()
+            time.sleep(1)
+            actions = ActionChains(browser)
+            actions.move_to_element(inputMensagem).click()
+            actions.send_keys(Keys.ENTER)
+            actions.perform()
+            time.sleep(1)
         else:
             print("Não foi possível localizar o campo de digitação.")
     except pyautogui.ImageNotFoundException as e:
@@ -60,9 +68,9 @@ def enviar_mensagem(mensagem):
         raise e
 
 
-# Iterar sobre os links de WhatsApp na planilha
+# Iterar sobre as mensagens de WhatsApp na planilha
 for i, mensagem in enumerate(planilha["MENSAGEM"]):
-    if pd.notnull(mensagem):  # Verifica se o link não está vazio
+    if pd.notnull(mensagem):  # Verifica se o campo de mensagem não está vazio
         mensagem_formatada = quote(mensagem)
         numero = planilha["NÚMERO"][i]
         link_whatsapp_web = (
@@ -72,18 +80,12 @@ for i, mensagem in enumerate(planilha["MENSAGEM"]):
 
         # Abrir o link no Opera GX (ou outro navegador)
         print("Abrindo o WhatsApp Web no navegador...")
-        webbrowser.get("chrome").open(link_whatsapp_web)
-        time.sleep(
-            10
-        )  # Espera a página carregar (pode ser ajustado conforme necessário)
+        browser.get(link_whatsapp_web)
 
         # Envia a mensagem da planilha para o whatsapp
         print(f"Enviando a mensagem: {mensagem}")
         enviar_mensagem(mensagem)
 
-        # Fechar a aba atual do navegador após enviar a mensagem
-        pyautogui.hotkey("ctrl", "w")  # Fecha a aba do navegador
-        time.sleep(5)  # Pausa para garantir o fechamento da aba
-
+browser.quit()  # Fechar o navegador
 # Mensagem final indicando que o processo foi concluído
 print("Todos os links foram processados e as mensagens foram enviadas.")
